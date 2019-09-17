e592ac4723f752902426e275b4e9889896a18eea
themountee1973 <royer.j@mac.com>
2019-09-17 16:22:57 -0700

    test

---
+++ web/services/states_service_add.php
@@ -0,0 +1,39 @@
+<?php
+	header('Content-type: application/json');
+
+	
+	if (!is_null($_POST)) {
+		
+		$usState = $_POST['usState'];
+		$capital = $_POST['capital'];
+		$abbr = $_POST['abbr'];
+		
+		$query = 'SELECT * FROM insertState(\''. $usState .'\', \''. $capital .'\', \''. $abbr .'\');';
+	
+		try {
+
+			$db = pg_connect("host=" . $params['host'] . " dbname=" . ltrim($params['path'], '/') . " user=" . $params['user'] . " password=" . $params['pass']);
+
+		}catch(Exception $e){
+			print_r('Exception:  ' . $e);
+		}
+		
+		$res = pg_query($db, $query);
+
+		$row = pg_fetch_array($res);
+		
+		$newRecId = $row[0];
+
+		$results['success'] = 1;
+		$results['recordId'] = $newRecId;
+
+	} else {
+		$results['success'] = 0;
+	}
+		
+	$redUrl = '/web/states.html';
+	
+	header('Location:' . $redUrl);
+	exit;
+
+?>
